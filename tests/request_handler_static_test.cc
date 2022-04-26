5ccde567c149fc35dea16da58f963e37f2900ead
Kevin Tang <kevintang2023@g.ucla.edu>
2022-04-25 17:06:04 -0700

    Added unit tests for static handler
    Change-Id: Icb396962eefb0aa1ad1d646c0bdde50341fe4c3a

--- tests/request_handler_static_test.cc
+++ tests/request_handler_static_test.cc
@@ -2,16 +2,35 @@
 #include "request_handler.hh"
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class StaticTest : public ::testing::Test {
  protected:
+  RequestHandlerStatic* a;
 
+  void SetUp() override {
+    a = new RequestHandlerStatic(".");
+  }
+
+  void TearDown() override {
+    delete a;
+  }
 
 };
 
-TEST_F(StaticTest, SuccessfulStart) {
-  RequestHandlerStatic a("");
-  cout << a.get_response("GET /small.png HTTP/1.1") << endl;
+TEST_F(StaticTest, Static200) {
+  std::string expected_response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: 14\r\n\r\ninvalid config";
+  EXPECT_EQ(expected_response, a->get_response("GET /invalid_config HTTP/1.1"));
+}
+
+TEST_F(StaticTest, Static400) {
+  std::string expected_response = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\nContent-Length: 37\r\n\r\n<html><h1>400 Bad Request</h1></html>";
+  EXPECT_EQ(expected_response, a->get_response("INVALID HTTP REQUEST"));
+}
+
+TEST_F(StaticTest, Static404) {
+  std::string expected_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: 35\r\n\r\n<html><h1>404 Not Found</h1></html>";
+  EXPECT_EQ(expected_response, a->get_response("GET /non_existing_file HTTP/1.1"));
 }

7a8a578073b78fc6a25581d83d35c61128adc7f8
Matthew Zhuo <mattzhuo@g.ucla.edu>
2022-05-10 23:52:06 +0000

    Added end to end test that exercises the 404 handler
    Change-Id: Ie63fceeb3d8dc62eaa3b031d5597e58cfd0fc2b6

---
+++ tests/404_test.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+./build/bin/server tests/request_handlers_config &
+SERVER_PID=$!
+sleep 1
+curl http://127.0.0.1/unknown -o ./returned_404 -s
+diff ./returned_404 tests/expected_404
+EQUIVALENT=$?
+rm ./returned_404
+kill $SERVER_PID
+exit $EQUIVALENT

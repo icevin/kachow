1020dc8559a55c066af768cd65c5c11ad4b8ee5b
Kevin Tang <kevintang2023@g.ucla.edu>
2022-06-03 02:48:50 -0700

    Add update script
    Change-Id: I617133b60311b4e0de533fc398200849ee817421

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+url=https://www.googleapis.com/oauth2/v1/certs
+
+mkdir keys > /dev/null 2>&1 || true
+
+while true
+do
+    wget $url -q -x -O ./keys/certs.json
+    INNER=0
+    while [ $INNER -lt 1 ]; do
+        sleep 60s
+        INNER=$((INNER + 1))
+    done
+done

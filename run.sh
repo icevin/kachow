5997bdec3698fb9e646e3760bac8aae3b5af51dd
Kevin Tang <kevintang2023@g.ucla.edu>
2022-06-03 19:52:38 -0700

    Deploy & coverage hotfixes
    Change-Id: I0a3fc155ce48f9d23c8200bcb553a4a0cbc705a5

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-wget https://www.googleapis.com/oauth2/v1/certs -x -O ./keys/certs.json &
+wget https://www.googleapis.com/oauth2/v1/certs -x -O ./keys/certs.json
 ./update.sh &
+PID=$!
 ./server $1
+CODE=$?
+kill $PID
+exit $CODE
